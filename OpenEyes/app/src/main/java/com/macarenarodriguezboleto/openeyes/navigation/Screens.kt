ab99f514a84b8a9248f5d75bd171e003411d21d7
Gabriel Narbona Benavente <macarenarodriguez.21@campuscamara.es>
2023-05-12 19:56:53 +0200

    MRB: Remake proyect for xml

--- OpenEyes/app/src/main/java/com/macarenarodriguezboleto/openeyes/navigation/Screens.kt
+++
@@ -1,6 +0,0 @@
-package com.macarenarodriguezboleto.openeyes.navigation
-
-sealed class Screens(val route: String) {
-    object Splash : Screens("splash_screen")
-    object Home : Screens("home_screen")
-}
